cd8cad71c63c30b13169b5ef613ef5383728d426
JamesK <James@10-geek.com>
2023-09-29 10:44:42 -0500

    Continue temporarly removal of Contact

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -20,18 +20,18 @@ const ContactForm = ({showForm, setShowForm}: ContactFormProps) => {
     e.preventDefault()
     // console.log('submitted')
     // console.log((e.target as HTMLFormElement).name.value)
-    const subjectData = `${(e.target as HTMLFormElement).name.value} - ${(e.target as HTMLFormElement).email.value}`
+    const subjectData = `${(e.target as HTMLFormElement).name.valueOf} - ${(e.target as HTMLFormElement).email.value}`
     // console.log((e.target as HTMLFormElement).email.value)
     const textData = (e.target as HTMLFormElement).message.value
     // console.log((e.target as HTMLFormElement).message.value)
     const htmlData = `
     <div style="padding: 24px; padding-right: 36px; border: 1px solid rgb(142, 142, 165); box-shadow: 1px 1px 1px rgb(142, 142, 165); font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif; border-radius: 5px; justify-content: center; width: fit-content; max-width: 50%; margin: 0 auto; background-color: rgb(240, 240, 240);">
         <h3 style="border: 1px solid rgb(63, 63, 75); color:rgb(48, 48, 51); padding: 5px 10px; border-radius: 5px; box-shadow: 1px 1px 1px rgb(142, 142, 165); width: fit-content; margin-left: 12px;">Name</h3>
-        <p style="margin-left: 24px; color:rgb(63, 63, 75);">${(e.target as HTMLFormElement).name.value}</p>
+        <p style="margin-left: 24px; color:rgb(63, 63, 75);">${(e.target as HTMLFormElement).name.valueOf}</p>
         <h3 style="border: 1px solid rgb(63, 63, 75); color:rgb(48, 48, 51); padding: 5px 10px; border-radius: 5px; box-shadow: 1px 1px 1px rgb(142, 142, 165); width: fit-content; margin-left: 12px;">Email</h3>
-        <p style="margin-left: 24px; color:rgb(63, 63, 75);">${(e.target as HTMLFormElement).email.value}</p>
+        <p style="margin-left: 24px; color:rgb(63, 63, 75);">${(e.target as HTMLFormElement).email.valueOf}</p>
         <h4 style="border: 1px solid rgb(63, 63, 75); color:rgb(48, 48, 51); padding: 5px 10px; border-radius: 5px; box-shadow: 1px 1px 1px rgb(142, 142, 165); width: fit-content; margin-left: 12px;">Message</h4>
-        <p style="margin-left: 24px; color:rgb(63, 63, 75);">${(e.target as HTMLFormElement).message.value}</p></div>
+        <p style="margin-left: 24px; color:rgb(63, 63, 75);">${(e.target as HTMLFormElement).message.valueOf}</p></div>
     </div>
     `
     // console.log((e.target as HTMLFormElement).attachment.files[0])
