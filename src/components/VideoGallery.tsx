90887f6f3deed483bb48aa308267251c44b5367f
KPInc425 <kpinc425@gmail.com>
2023-09-29 20:53:12 -0700

    Update video gallery style

--- src/components/VideoGallery.tsx
+++ src/components/VideoGallery.tsx
@@ -18,7 +18,7 @@ import { Box, Link, Flex, Embed, Button } from "theme-ui";
 
 const VideoGallery = () => {
   return (
-    <Box py={['4','5']} className="terms-gradient">
+    <Box py={['4','5']} sx={{backgroundColor: 'text'}}>
       <Box mx="auto" sx={{ width: "80vw", textAlign: "center" }}>
         <Flex pt='4' sx={{ justifyContent: "center", gap: "5" }}>
           <Embed src="https://www.youtube.com/embed/7eZkuLe7_Dk" />
